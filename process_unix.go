b93baa95a0fa6a6465f6d97676f02eb167c3f792
Sebastien Binet <binet@cern.ch>
2024-11-14 14:41:31 +0100

    all: add documentation for ClockTicks and PageSize
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- process_unix.go
+++ process_unix.go
@@ -8,9 +8,15 @@ import (
 	"os"
 )
 
-var (
+const (
+	// ClockTicks is the number of clock ticks per second.
+	// ClockTicks is a constant on Linux and Darwin.
 	ClockTicks = uint64(100) // uint64(C.sysconf(C._SC_CLK_TCK))
-	PageSize   = int64(os.Getpagesize())
 
 	clockTicksToNanosecond = (1000000000 / ClockTicks)
+)
+
+var (
+	// PageSize is the underlying system's memory page size.
+	PageSize = int64(os.Getpagesize())
 )
